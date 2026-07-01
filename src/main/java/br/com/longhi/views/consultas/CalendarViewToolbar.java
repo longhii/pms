b969eead5258d81fd7bfa5eb7754be80d5132a32
Gabriel Longhi <gabrielkuodrek@hotmail.com>
2026-07-01 02:23:21 -0300

    feat: CRUD de consultas com FullCalendar, formato lista mensal e layout responsivo
    - Adiciona tela de consultas como view inicial com FullCalendar (listMonth)
    - Implementa CRUD completo: criar/editar/excluir consultas via Dialog
    - Adiciona toolbar com navegação (anterior/próximo/hoje) e troca de views (Mês/Semana/Dia/Lista)
    - Configura formato de data personalizado no list view: '1 de julho de 2026' com dia da semana à direita
    - Layout responsivo: toolbar com wrap em mobile, calendário ocupa largura total
    - Atualiza data.sql com consultas em junho/julho 2026
    - Corrige binder.getBean() null no dialog de criação

--- src/main/java/br/com/longhi/views/consultas/CalendarViewToolbar.java
+++ src/main/java/br/com/longhi/views/consultas/CalendarViewToolbar.java
@@ -1,12 +1,14 @@
 package br.com.longhi.views.consultas;
 
-import com.vaadin.flow.component.HasText;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.datepicker.DatePicker;
+import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.icon.VaadinIcon;
-import com.vaadin.flow.component.menubar.MenuBar;
-import org.vaadin.stefan.fullcalendar.CalendarView;
+import com.vaadin.flow.component.orderedlayout.FlexComponent.JustifyContentMode;
+import com.vaadin.flow.component.orderedlayout.FlexLayout;
+import com.vaadin.flow.component.orderedlayout.FlexLayout.FlexWrap;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import org.vaadin.stefan.fullcalendar.CalendarViewImpl;
 import org.vaadin.stefan.fullcalendar.FullCalendar;
 
@@ -14,46 +16,72 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.Locale;
 
-public class CalendarViewToolbar extends MenuBar {
+public class CalendarViewToolbar extends HorizontalLayout {
 
     private final FullCalendar calendar;
-    private Button buttonDatePicker;
-    private CalendarView selectedView = CalendarViewImpl.DAY_GRID_MONTH;
+    private final Button buttonDatePicker;
 
     public CalendarViewToolbar(FullCalendar calendar) {
         this.calendar = calendar;
-        initDateItems();
-        this.calendar.addDatesRenderedListener(event -> this.updateInterval(event.getIntervalStart()));
 
-    }
+        setWidthFull();
+        setPadding(true);
+        setSpacing(true);
+        setAlignItems(Alignment.CENTER);
 
-    private void initDateItems() {
-        addItem(VaadinIcon.ANGLE_LEFT.create(), e -> calendar.previous()).setId("period-previous-button");
-
+        buttonDatePicker = new Button();
+        buttonDatePicker.addThemeVariants(ButtonVariant.LUMO_TERTIARY_INLINE);
         var gotoDate = new DatePicker();
-        gotoDate.addValueChangeListener(event1 -> calendar.gotoDate(event1.getValue()));
-        gotoDate.getElement().getStyle().set("visibility", "hidden");
-        gotoDate.getElement().getStyle().set("position", "fixed");
+        gotoDate.addValueChangeListener(event1 -> {
+            if (event1.getValue() != null) {
+                calendar.gotoDate(event1.getValue());
+            }
+        });
+        gotoDate.setVisible(false);
         gotoDate.setWidth("0px");
         gotoDate.setHeight("0px");
-        gotoDate.setWeekNumbersVisible(true);
-        buttonDatePicker = new Button(VaadinIcon.CALENDAR.create());
-        buttonDatePicker.addThemeVariants(ButtonVariant.LUMO_TERTIARY_INLINE);
         buttonDatePicker.getElement().appendChild(gotoDate.getElement());
         buttonDatePicker.addClickListener(event -> gotoDate.open());
-        buttonDatePicker.setWidthFull();
-        addItem(buttonDatePicker);
-        addItem(VaadinIcon.ANGLE_RIGHT.create(), e -> calendar.next());
+
+        var previousButton = new Button(VaadinIcon.ANGLE_LEFT.create(), e -> calendar.previous());
+        previousButton.addThemeVariants(ButtonVariant.LUMO_TERTIARY_INLINE);
+        var nextButton = new Button(VaadinIcon.ANGLE_RIGHT.create(), e -> calendar.next());
+        nextButton.addThemeVariants(ButtonVariant.LUMO_TERTIARY_INLINE);
+
+        var viewMonth = new Button("M\u00eas", e -> calendar.changeView(CalendarViewImpl.DAY_GRID_MONTH));
+        viewMonth.addThemeVariants(ButtonVariant.LUMO_SMALL, ButtonVariant.LUMO_TERTIARY);
+        var viewWeek = new Button("Semana", e -> calendar.changeView(CalendarViewImpl.TIME_GRID_WEEK));
+        viewWeek.addThemeVariants(ButtonVariant.LUMO_SMALL, ButtonVariant.LUMO_TERTIARY);
+        var viewDay = new Button("Dia", e -> calendar.changeView(CalendarViewImpl.TIME_GRID_DAY));
+        viewDay.addThemeVariants(ButtonVariant.LUMO_SMALL, ButtonVariant.LUMO_TERTIARY);
+        var viewListMonth = new Button("Lista", e -> calendar.changeView(CalendarViewImpl.LIST_MONTH));
+        viewListMonth.addThemeVariants(ButtonVariant.LUMO_SMALL, ButtonVariant.LUMO_TERTIARY);
+
+        var dateNav = new HorizontalLayout(previousButton, buttonDatePicker, nextButton);
+        dateNav.setAlignItems(Alignment.CENTER);
+        dateNav.setPadding(false);
+        dateNav.setSpacing(false);
+
+        var views = new FlexLayout(viewMonth, viewWeek, viewDay, viewListMonth);
+        views.setAlignItems(Alignment.CENTER);
+        views.setFlexWrap(FlexWrap.WRAP);
+        views.setJustifyContentMode(JustifyContentMode.END);
+        views.getStyle().set("gap", "0");
+
+        var spacer = new Div();
+        spacer.getStyle().set("flex", "1");
+
+        add(dateNav, spacer, views);
+        setAlignItems(Alignment.CENTER);
+
+        calendar.addDatesRenderedListener(event -> updateInterval(event.getIntervalStart()));
     }
 
     public void updateInterval(LocalDate intervalStart) {
-        if (buttonDatePicker != null && selectedView != null) {
-            updateIntervalLabel(buttonDatePicker, selectedView, intervalStart);
+        if (buttonDatePicker != null) {
+            Locale locale = calendar.getLocale();
+            buttonDatePicker.setText(intervalStart.format(
+                    DateTimeFormatter.ofPattern("MMMM yyyy").withLocale(locale)));
         }
-    }
-
-    void updateIntervalLabel(HasText intervalLabel, CalendarView view, LocalDate intervalStart) {
-        Locale locale = calendar.getLocale();
-        intervalLabel.setText(intervalStart.format(DateTimeFormatter.ofPattern("MMMM yyyy").withLocale(locale)));
     }
 }
--- src/main/java/br/com/longhi/views/consultas/ConsultasView.java
+++ src/main/java/br/com/longhi/views/consultas/ConsultasView.java
@@ -1,31 +1,42 @@
 package br.com.longhi.views.consultas;
 
 import br.com.longhi.data.Consulta;
-import br.com.longhi.services.ConsultaService;
+import br.com.longhi.data.Paciente;
+import br.com.longhi.data.Status;
+import br.com.longhi.repository.ConsultaRepository;
+import br.com.longhi.repository.PacienteRepository;
+import br.com.longhi.security.AuthenticatedUser;
 import br.com.longhi.views.MainLayout;
-import com.vaadin.flow.component.Composite;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.combobox.ComboBox;
+import com.vaadin.flow.component.confirmdialog.ConfirmDialog;
+import com.vaadin.flow.component.datepicker.DatePicker;
 import com.vaadin.flow.component.dependency.Uses;
+import com.vaadin.flow.component.dialog.Dialog;
+import com.vaadin.flow.component.formlayout.FormLayout;
 import com.vaadin.flow.component.icon.Icon;
-import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.icon.VaadinIcon;
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.orderedlayout.FlexComponent.Alignment;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.select.Select;
+import com.vaadin.flow.component.timepicker.TimePicker;
+import com.vaadin.flow.data.binder.Binder;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.router.RouteAlias;
-import com.vaadin.flow.server.auth.AnonymousAllowed;
 import elemental.json.Json;
 import jakarta.annotation.security.PermitAll;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.vaadin.stefan.fullcalendar.*;
-import org.vaadin.stefan.fullcalendar.dataprovider.CallbackEntryProvider;
+import org.vaadin.stefan.fullcalendar.Entry;
+import org.vaadin.stefan.fullcalendar.FullCalendar;
+import org.vaadin.stefan.fullcalendar.FullCalendarBuilder;
+import org.vaadin.stefan.fullcalendar.Timezone;
 import org.vaadin.stefan.fullcalendar.dataprovider.EntryProvider;
-import org.vaadin.stefan.fullcalendar.dataprovider.EntryQuery;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.time.ZoneId;
-import java.time.temporal.TemporalAdjuster;
-import java.time.temporal.TemporalAdjusters;
-import java.util.Locale;
 
 @PageTitle("Consultas")
 @Route(value = "", layout = MainLayout.class)
@@ -34,28 +45,274 @@ import java.util.Locale;
 @Uses(Icon.class)
 public class ConsultasView extends VerticalLayout {
 
-    public ConsultasView(ConsultaService consultaService) {
+    private final ConsultaRepository consultaRepository;
+    private final PacienteRepository pacienteRepository;
+    private final AuthenticatedUser authenticatedUser;
+    private FullCalendar calendar;
+    private EntryProvider<Entry> entryProvider;
+
+    @Autowired
+    public ConsultasView(ConsultaRepository consultaRepository,
+                         PacienteRepository pacienteRepository,
+                         AuthenticatedUser authenticatedUser) {
+        this.consultaRepository = consultaRepository;
+        this.pacienteRepository = pacienteRepository;
+        this.authenticatedUser = authenticatedUser;
+
         setSizeFull();
+        setPadding(true);
+        setSpacing(true);
 
         var initialOptions = Json.createObject();
         initialOptions.put("initialView", "listMonth");
+        initialOptions.put("locale", "pt-br");
+        initialOptions.put("firstDay", 0);
+        initialOptions.put("height", "auto");
 
-        CallbackEntryProvider<Entry> call = EntryProvider.fromCallbacks(
-                query -> consultaService.streamEntries(query).stream(),
-                entryId -> consultaService.getEntry(entryId).orElse(null));
+        var headerToolbar = Json.createObject();
+        headerToolbar.put("start", "");
+        headerToolbar.put("center", "");
+        headerToolbar.put("end", "");
+        initialOptions.put("headerToolbar", headerToolbar);
 
-        FullCalendar calendar = FullCalendarBuilder.create()
+        var viewsOpts = Json.createObject();
+        var listDayFormat = Json.createObject();
+        listDayFormat.put("month", "long");
+        listDayFormat.put("day", "numeric");
+        listDayFormat.put("year", "numeric");
+        var listDaySideFormat = Json.createObject();
+        listDaySideFormat.put("weekday", "long");
+        for (var view : new String[]{"listMonth", "listWeek", "listDay"}) {
+            var viewOpts = Json.createObject();
+            viewOpts.put("listDayFormat", listDayFormat);
+            viewOpts.put("listDaySideFormat", listDaySideFormat);
+            viewsOpts.put(view, viewOpts);
+        }
+        initialOptions.put("views", viewsOpts);
+
+        entryProvider = EntryProvider.fromCallbacks(
+                query -> {
+                    var psi = authenticatedUser.carregarPsicologoLogado();
+                    return consultaRepository.findByPacientePsicologoAndDataBetween(
+                            psi,
+                            query.getStart().toLocalDate(),
+                            query.getEnd().toLocalDate()
+                    ).stream().map(Consulta::toEntry);
+                },
+                entryId -> consultaRepository
+                        .findById(Long.valueOf(entryId))
+                        .map(Consulta::toEntry)
+                        .orElse(null));
+
+        calendar = FullCalendarBuilder.create()
                 .withInitialOptions(initialOptions)
-                .withEntryProvider(call)
+                .withEntryProvider(entryProvider)
                 .build();
 
         calendar.setTimezone(new Timezone(ZoneId.of("America/Sao_Paulo")));
 
+        calendar.addEntryClickedListener(event -> {
+            var consulta = consultaRepository.findById(
+                    Long.valueOf(event.getEntry().getId())).orElse(null);
+            if (consulta != null) {
+                abrirDialogEditar(consulta);
+            }
+        });
+
+        calendar.addTimeslotClickedListener(event -> {
+            var data = event.getDate();
+            if (data != null) {
+                abrirDialogCriar(data);
+            }
+        });
+
         CalendarViewToolbar toolbar = new CalendarViewToolbar(calendar);
 
-        add(toolbar);
-        add(calendar);
+        var addButton = new Button("Nova consulta", VaadinIcon.PLUS.create());
+        addButton.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+        addButton.addClickListener(e -> abrirDialogCriar(LocalDate.now()));
+
+        var header = new VerticalLayout(toolbar, addButton);
+        header.setPadding(false);
+        header.setSpacing(true);
+        header.setWidthFull();
+
+        add(header, calendar);
         setFlexGrow(1, calendar);
         setHorizontalComponentAlignment(Alignment.STRETCH, calendar);
+    }
+
+    private void abrirDialogCriar(LocalDate dataSugerida) {
+        var dialog = new Dialog();
+        dialog.setHeaderTitle("Nova consulta");
+        dialog.setWidth("500px");
+
+        var binder = new Binder<>(Consulta.class);
+
+        var pacienteField = new ComboBox<Paciente>("Paciente");
+        pacienteField.setItems(pacienteRepository.findByPsicologo(
+                authenticatedUser.carregarPsicologoLogado()));
+        pacienteField.setItemLabelGenerator(Paciente::getNome);
+        pacienteField.setRequired(true);
+        binder.forField(pacienteField)
+                .asRequired("Selecione um paciente")
+                .bind(Consulta::getPaciente, Consulta::setPaciente);
+
+        var dataField = new DatePicker("Data");
+        dataField.setValue(dataSugerida);
+        dataField.setRequired(true);
+        binder.forField(dataField)
+                .asRequired("Selecione uma data")
+                .bind(Consulta::getData, Consulta::setData);
+
+        var horaInicioField = new TimePicker("Hora in\u00edcio");
+        horaInicioField.setRequired(true);
+        binder.forField(horaInicioField)
+                .asRequired("Informe o hor\u00e1rio de in\u00edcio")
+                .bind(Consulta::getHoraInicio, Consulta::setHoraInicio);
+
+        var horaFimField = new TimePicker("Hora fim");
+        horaFimField.setRequired(true);
+        binder.forField(horaFimField)
+                .asRequired("Informe o hor\u00e1rio de t\u00e9rmino")
+                .bind(Consulta::getHoraFim, Consulta::setHoraFim);
+
+        var statusField = new Select<Status>();
+        statusField.setLabel("Status");
+        statusField.setItems(Status.values());
+        statusField.setItemLabelGenerator(s -> {
+            return switch (s) {
+                case ATENDIDO -> "Atendido";
+                case CANCELADO -> "Cancelado";
+                case EM_ESPERA -> "Em espera";
+            };
+        });
+        statusField.setValue(Status.EM_ESPERA);
+        binder.forField(statusField)
+                .asRequired("Selecione um status")
+                .bind(Consulta::getStatus, Consulta::setStatus);
+
+        var consulta = new Consulta();
+        binder.setBean(consulta);
+
+        var form = new FormLayout(pacienteField, dataField, horaInicioField, horaFimField, statusField);
+
+        var salvarButton = new Button("Salvar", e -> {
+            if (binder.validate().isOk()) {
+                try {
+                    consultaRepository.save(consulta);
+                    entryProvider.refreshAll();
+                    Notification.show("Consulta salva com sucesso.");
+                    dialog.close();
+                } catch (Exception ex) {
+                    Notification.show("Erro ao salvar consulta: " + ex.getMessage());
+                }
+            } else {
+                Notification.show("Verifique os campos obrigatórios.");
+            }
+        });
+        salvarButton.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+
+        var cancelarButton = new Button("Cancelar", e -> dialog.close());
+
+        dialog.add(form);
+        dialog.getFooter().add(cancelarButton, salvarButton);
+        dialog.open();
+    }
+
+    private void abrirDialogEditar(Consulta consulta) {
+        var dialog = new Dialog();
+        dialog.setHeaderTitle("Editar consulta");
+        dialog.setWidth("500px");
+
+        var binder = new Binder<>(Consulta.class);
+
+        var pacienteField = new ComboBox<Paciente>("Paciente");
+        pacienteField.setItems(pacienteRepository.findByPsicologo(
+                authenticatedUser.carregarPsicologoLogado()));
+        pacienteField.setItemLabelGenerator(Paciente::getNome);
+        pacienteField.setRequired(true);
+        binder.forField(pacienteField)
+                .asRequired("Selecione um paciente")
+                .bind(Consulta::getPaciente, Consulta::setPaciente);
+
+        var dataField = new DatePicker("Data");
+        dataField.setRequired(true);
+        binder.forField(dataField)
+                .asRequired("Selecione uma data")
+                .bind(Consulta::getData, Consulta::setData);
+
+        var horaInicioField = new TimePicker("Hora in\u00edcio");
+        horaInicioField.setRequired(true);
+        binder.forField(horaInicioField)
+                .asRequired("Informe o hor\u00e1rio de in\u00edcio")
+                .bind(Consulta::getHoraInicio, Consulta::setHoraInicio);
+
+        var horaFimField = new TimePicker("Hora fim");
+        horaFimField.setRequired(true);
+        binder.forField(horaFimField)
+                .asRequired("Informe o hor\u00e1rio de t\u00e9rmino")
+                .bind(Consulta::getHoraFim, Consulta::setHoraFim);
+
+        var statusField = new Select<Status>();
+        statusField.setLabel("Status");
+        statusField.setItems(Status.values());
+        statusField.setItemLabelGenerator(s -> {
+            return switch (s) {
+                case ATENDIDO -> "Atendido";
+                case CANCELADO -> "Cancelado";
+                case EM_ESPERA -> "Em espera";
+            };
+        });
+        binder.forField(statusField)
+                .asRequired("Selecione um status")
+                .bind(Consulta::getStatus, Consulta::setStatus);
+
+        var form = new FormLayout(pacienteField, dataField, horaInicioField, horaFimField, statusField);
+        binder.setBean(consulta);
+
+        var salvarButton = new Button("Salvar", e -> {
+            if (binder.validate().isOk()) {
+                try {
+                    consultaRepository.save(binder.getBean());
+                    entryProvider.refreshAll();
+                    Notification.show("Consulta atualizada com sucesso.");
+                    dialog.close();
+                } catch (Exception ex) {
+                    Notification.show("Erro ao atualizar consulta: " + ex.getMessage());
+                }
+            } else {
+                Notification.show("Verifique os campos obrigat\u00f3rios.");
+            }
+        });
+        salvarButton.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+
+        var excluirButton = new Button("Excluir", VaadinIcon.TRASH.create(), e -> {
+            var confirm = new ConfirmDialog();
+            confirm.setHeader("Excluir consulta");
+            confirm.setText("Tem certeza que deseja excluir esta consulta?");
+            confirm.setCancelable(true);
+            confirm.setCancelText("Cancelar");
+            confirm.setConfirmText("Excluir");
+            confirm.setConfirmButtonTheme("error primary");
+            confirm.addConfirmListener(ev -> {
+                try {
+                    consultaRepository.delete(consulta);
+                    entryProvider.refreshAll();
+                    Notification.show("Consulta exclu\u00edda.");
+                    dialog.close();
+                } catch (Exception ex) {
+                    Notification.show("Erro ao excluir consulta: " + ex.getMessage());
+                }
+            });
+            confirm.open();
+        });
+        excluirButton.addThemeVariants(ButtonVariant.LUMO_ERROR);
+
+        var cancelarButton = new Button("Cancelar", e -> dialog.close());
+
+        dialog.add(form);
+        dialog.getFooter().add(cancelarButton, excluirButton, salvarButton);
+        dialog.open();
     }
 }
